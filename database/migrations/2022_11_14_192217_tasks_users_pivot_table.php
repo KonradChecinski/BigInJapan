7fed5cd5d1d9f26fb3f51eb104cd515e391aec2d
KonradChecinski <72439528+KonradChecinski@users.noreply.github.com>
2022-12-12 22:16:18 +0100

    Pierwotna wersja

---
+++ database/migrations/2022_11_14_192217_tasks_users_pivot_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks_users_pivot', function (Blueprint $table){
+           $table->foreignId('tasks_id');
+           $table->foreignId('users_id');
+           $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks_users_pivot');
+    }
+};
