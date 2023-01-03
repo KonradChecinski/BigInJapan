8cac5e20fd31d238dffee80328ee7c375246064d
Gromzi <mamgier@gmail.com>
2023-01-03 14:51:18 +0100

    Change styling of button on login screen

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -19,7 +19,7 @@ const LoginComponent = () => {
 
       <View style={styles.logInButtonContainer}>
         <Text
-          style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}
+          style={{ color: 'white', fontSize: 15, fontWeight: 'bold' }}
         >
           Zaloguj się
         </Text>
@@ -35,9 +35,9 @@ export default LoginComponent
 const styles = StyleSheet.create({
   main: {
     justifyContent: 'space-between',
-    marginBottom: 75,
+    marginBottom: 60,
     width: 300,
-    height: 150,
+    height: 200,
   },
   inputContainer: {
     justifyContent: 'space-between',
@@ -49,7 +49,14 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   logInButtonContainer: {
-    marginTop: 60,
+    width: 300,
+    marginTop: 50,
+    padding: 10,
     alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: '#161A1D',
+    borderWidth: 4,
+    borderRadius: 10,
+    borderColor: '#161A1D',
   },
 })
